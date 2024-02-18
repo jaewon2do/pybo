de71abc18e0c98c69d7ae197e2080c2b4d5dc87c
jaewonlee <wodnjsgidrl@gmail.com>
2024-02-18 23:38:30 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#)@()98s(#5_(nzu*-@+n-z80d(xi3##ohhwpolfxg+d3jc85q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.241.109']
 
 
 # Application definition
